bab7d1a66f28961df3b9a6512a946fe132da37a3
kalyandechiraju <kalyandechiraju@outlook.com>
2019-05-21 11:43:48 +0530

    Initial commit

---
+++ app/src/main/java/in/kalyan/storedemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package `in`.kalyan.storedemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.nytimes.android.external.store3.base.impl.Store
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Store
+    }
+}
